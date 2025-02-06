6cf48c12a388a246f2541e46c9b7398b1885a3fa
teewijit <wijit_777@hotmail.co.th>
2025-02-06 16:51:10 +0700

    (fix):port

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.get("/", (req, res) => {
 });
 
 // เริ่มเซิร์ฟเวอร์
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 25680;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
